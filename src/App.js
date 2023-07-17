6ee7e37fca829703171c5bf4e10cfeab348d1321
MSI\kkddy <kkddy1206@naver.com>
2023-07-17 14:08:32 +0900

    props를 통해 컴포넌트에게 값 전달하기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import Hello from './Hello';
+// import Hello from './Hello';
+import Hello_1 from './Hello';
 import './App.css';
+import Wrapper from './Wrapper';
 
-function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding:'1rem'
-  };
-  return ( //JSX사용법
-  <> {/* 두개 이상의 태그는 이렇게 감싸줘야한다*/}
-    {/*주석쓰는 방법*/}
-    <Hello 
-    //또는 이런식으로 작성하는 주석도 있다
-    /> 
-    <div>안녕히계세요.</div>
-    <div style={style}>{name}</div> {/*자바스크립트 값 보여줄 땐 중괄호로 감싸줘야한다*/}
-    <div className="gray-box"></div>
-  </> //리액트에서는 태그는 꼭 당혀야 한다
+// //JSX사용법
+// function App() {
+//   const name = 'react';
+//   const style = {
+//     backgroundColor: 'black',
+//     color: 'aqua',
+//     fontSize: 24,
+//     padding:'1rem'
+//   };
+//   return ( 
+//   <> {/* 두개 이상의 태그는 이렇게 감싸줘야한다*/}
+//     {/*주석쓰는 방법*/}
+//     <Hello 
+//     //또는 이런식으로 작성하는 주석도 있다
+//     /> 
+//     <div>안녕히계세요.</div>
+//     <div style={style}>{name}</div> {/*자바스크립트 값 보여줄 땐 중괄호로 감싸줘야한다*/}
+//     <div className="gray-box"></div>
+//   </> //리액트에서는 태그는 꼭 당혀야 한다
+//   );
+// }
+
+// export default App;
+
+//porps
+function App_1() {
+  return ( 
+    <Wrapper> {/*Hello_1컴포넌트 두개가 레퍼 안에 위치*/}
+    <Hello_1 name="react" color="red" />
+    <Hello_1 color="pink" />
+    </Wrapper>
   );
 }
 
-export default App;
+export default App_1;
+
+
+
--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,31 @@
 import React from 'react'; //리엑트를 불러와서 사용하겠다
 
-//컴포넌트 만들기
-function Hello(){
-    return <div>안녕하세요<button>랄랄</button></div>
+// //컴포넌트 만들기
+// function Hello(){
+//     return <div>안녕하세요<button>랄랄</button></div>
+// }
+// export default Hello; //Hello라는 컴포넌트를 만들어서 내보내주겠다
+
+
+//props 기본
+// function Hello_1(props){
+//     console.log(props);
+//     return <div style={{
+//         color : props.color
+//     }}>안녕하세요{props.name}</div>
+// }
+
+// props구조분해
+function Hello_1({color, name}){
+    return <div style={{
+        // color:color
+        color
+    }}>안녕하세요 {name}</div>
 }
 
-export default Hello; //Hello라는 컴포넌트를 만들어서 내보내주겠다
+//특정값을 빠트렸을 때 기본으로 사용할 값
+Hello_1.defaultProps = {
+    name : '이름없음'
+};
+
+export default Hello_1; //Hello라는 컴포넌트를 만들어서 내보내주겠다
